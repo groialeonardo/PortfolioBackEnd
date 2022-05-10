5252f69ff2ed3fa005138d1fb2741c3dfafd95cf
groialeonardo <51366574+groialeonardo@users.noreply.github.com>
2022-05-10 15:51:32 -0300

    se agrega el ath controller
    se agrega auth controller y Usuario DTO

---
+++ src/main/java/com/groialeonardo/lgportfolio/controller/AuthenticationController.java
@@ -0,0 +1,21 @@
+ 
+package com.groialeonardo.lgportfolio.controller;
+
+import com.groialeonardo.lgportfolio.DTO.UsuarioDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@CrossOrigin
+@RestController
+public class AuthenticationController {
+    
+    @GetMapping(produces = "application/json")
+    @RequestMapping( "/auth" )
+    public UsuarioDTO validateLogin() {
+	return new UsuarioDTO("User successfully authenticated");                
+    }
+    
+}
